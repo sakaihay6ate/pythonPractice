3ea4b827f3c5f49754de0c0c2affd483340659f5
RueichiLin-PC\RueichiLin <rueichilin@astrocorp.com.tw>
2019-08-21 10:24:09 +0800

    create

---
+++ tkintertest.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window=tk.Tk()
+window.title('python window practice')
+window.geometry('960x540')
+l=tk.Label(window, text='test text',width=30,height=20)
+l.pack()
+window.mainloop()
